3dbd274b113c9a8b925feead95e9653df9c6d1b6
Inchino <alessandro.incalza22@gmail.com>
2024-12-12 17:06:19 +0100

    ES. 2

--- assets/js/CRUD.js
+++ assets/js/CRUD.js
@@ -17,6 +17,9 @@ fetch(`https://api.pexels.com/v1/search?query=mountain&per_page=9`,{
 })
 
 const btnLoadImages = document.getElementById('loadImages');
+const btnLoadSecondaryImages = document.getElementById('loadSecondaryImages');
+
+/*------ ESERCIZIO 1 ------*/
 
 btnLoadImages.addEventListener('click', function (e) {
     e.preventDefault();
@@ -28,6 +31,33 @@ function LoadImages(){
         method: 'GET',
         headers: {
             'Authorization': 'MqyJQlhAUKzcYIWiyCF6BNqSqRFPR1x7DRjQJOt4oinE2TdIFWy9ULT7'
+        },
+    })
+    .then(response=>{
+        console.log('response', response)
+        if(response.ok){
+            return response.json()
+        }else{
+            throw new Error('Errore nella chiamata')
+        }
+    })
+    .catch((error)=>{
+        console.log('error', error)
+    })
+}
+
+/*------ ESERCIZIO 2 ------*/
+
+btnLoadSecondaryImages.addEventListener('click', function (e) {
+    e.preventDefault();
+    LoadSecondaryImages();
+})
+
+function LoadSecondaryImages(){
+    fetch(`https://api.pexels.com/v1/search?query=[your-secondary-query]`,{
+        method: 'GET',
+        headers: {
+            'Authorization' : 'MqyJQlhAUKzcYIWiyCF6BNqSqRFPR1x7DRjQJOt4oinE2TdIFWy9ULT7'
         },
     })
     .then(response=>{
