19e39e4d6f3d5e88789499a227c31016bf346e38
Inchino <alessandro.incalza22@gmail.com>
2024-12-12 16:59:34 +0100

    Es. 1

--- assets/js/CRUD.js
+++ assets/js/CRUD.js
@@ -1,7 +1,7 @@
-fetch(`https://www.pexels.com/it-it/api`,{
+fetch(`https://api.pexels.com/v1/search?query=mountain&per_page=9`,{
     method: 'GET',
     headers: {
-        Authorization: 'MqyJQlhAUKzcYIWiyCF6BNqSqRFPR1x7DRjQJOt4oinE2TdIFWy9ULT7'
+        'Authorization' : 'MqyJQlhAUKzcYIWiyCF6BNqSqRFPR1x7DRjQJOt4oinE2TdIFWy9ULT7'
     },
 })
 .then(response=>{
@@ -9,9 +9,36 @@ fetch(`https://www.pexels.com/it-it/api`,{
     if(response.ok){
         return response.json()
     }else{
-        return new Error('Errore nella chiamata')
+        throw new Error('Errore nella chiamata')
     }
 })
 .catch((error)=>{
     console.log('error', error)
 })
+
+const btnLoadImages = document.getElementById('loadImages');
+
+btnLoadImages.addEventListener('click', function (e) {
+    e.preventDefault();
+    LoadImages();
+})
+
+function LoadImages(){
+    fetch(`https://api.pexels.com/v1/search?query=[your-query]`,{
+        method: 'GET',
+        headers: {
+            'Authorization': 'MqyJQlhAUKzcYIWiyCF6BNqSqRFPR1x7DRjQJOt4oinE2TdIFWy9ULT7'
+        },
+    })
+    .then(response=>{
+        console.log('response', response)
+        if(response.ok){
+            return response.json()
+        }else{
+            throw new Error('Errore nella chiamata')
+        }
+    })
+    .catch((error)=>{
+        console.log('error', error)
+    })
+}
